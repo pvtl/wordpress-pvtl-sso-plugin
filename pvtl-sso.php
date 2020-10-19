a95b39ba7172b2ecf2df5430b55d1692bd32f74a
Matt McNamee <matt@mcnamee.co>
2020-10-20 09:22:39 +1000

    Bump version to 1.1.0

--- pvtl-sso.php
+++ pvtl-sso.php
@@ -7,7 +7,7 @@
  * Author URI:      http://pivotal.agency
  * Text Domain:     pvtl-sso
  * Domain Path:     /languages
- * Version:         1.0.6
+ * Version:         1.1.0
  *
  * @package         PVTL_SSO
  */
